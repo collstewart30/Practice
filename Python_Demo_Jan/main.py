7b0e70f73b6762e5f4aa10f1ac23fae0c7387700
Colleen Stewart <collstewart30@gmail.com>
2023-01-24 16:47:40 -0500

    Added Python_Demo_Jan to Practice repository

---
+++ Python_Demo_Jan/main.py
@@ -0,0 +1,35 @@
+# greeting = ('Hello world')
+
+# cheezits_in_hand = 25
+# cheezits_in_box = 1000
+
+# print(cheezits_in_box)
+# cheezits_in_box -= cheezits_in_hand
+# print(cheezits_in_box)
+
+# print(greeting)
+
+# age = input('How old are you? ')
+# print(f'I am {age} years old.')
+
+# first_name = input('What is your first name? ')
+# last_name = input('What is your last name? ')
+# full_name = first_name + " " + last_name
+# print(f'My first name is {first_name} and my last name is {last_name} which means my full name is {full_name}')
+
+
+# fahrenheit = input('What degree F is it? ')
+# celsius = (int(fahrenheit) - 32) * (5/9)
+# print(f'{fahrenheit} degrees Fahrenheit is {celsius} degrees Celsius')
+
+# driving_age = str(16)
+
+# user_age = input('How old are you? ')
+
+# def can_drive():
+#     if user_age >= driving_age:
+#         print('You are legally able to drive')
+#     else:
+#         print('You are not old enough to drive yet')
+
+# can_drive()
